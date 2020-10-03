97f6df197ecac4b7e1753006fb54dd8be361d215
Michael Byalsky <michael.byalsky@gmail.com>
2020-10-03 11:27:41 +0300

    hospitals test

---
+++ server/tests/hospital.test.js
@@ -0,0 +1,63 @@
+const request = require("supertest");
+const app = require("../app");
+
+//model for bulkCreate
+require('mysql2/node_modules/iconv-lite').encodingExists('foo');
+
+const {
+  Patients,
+  CovidTests,
+  Symptoms,
+  SymptomsByPatients,
+  Cities,
+  Hospitals
+} = require("../models");
+
+//mock data
+const patientsMock = require('./mockData/patientMock')
+const covidTestMock = require('./mockData/covidTestMock')
+const citiesMock = require('./mockData/citiesMock')
+const symptomMock = require('./mockData/symptomsMock')
+const SymptomsByPatientsMock = require('./mockData/symptomsByPatientMock')
+const hospitalsMock = require("./mockData/hospitalsMock");
+
+describe("Patient api tests", () => {
+
+    beforeAll(async () => {
+      console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+      await Patients.destroy({ truncate: true, force: true });
+      await CovidTests.destroy({ truncate: true, force: true });
+      await Symptoms.destroy({ truncate: true, force: true });
+      await SymptomsByPatients.destroy({ truncate: true, force: true });
+      await Cities.destroy({ truncate: true, force: true });
+    });
+  
+    afterAll(async () => {
+     app.close()
+    })
+  
+    it("Can add new patients, cities, symptoms, hospitals and covid tests", async () => {
+      
+      const patientsResult = await Patients.bulkCreate(patientsMock)
+      expect(patientsResult.length).toBe(5)
+  
+      const covidTestResult = await CovidTests.bulkCreate(covidTestMock)
+      expect(covidTestResult.length).toBe(5)
+  
+      const citiesResult = await Cities.bulkCreate(citiesMock)
+      expect(citiesResult.length).toBe(2)
+  
+      const symptomsResult = await Symptoms.bulkCreate(symptomMock)
+      expect(symptomsResult.length).toBe(3)
+      
+      const symptomsByPatientsResult = await SymptomsByPatients.bulkCreate(SymptomsByPatientsMock)
+      expect(symptomsByPatientsResult.length).toBe(5)
+      
+      const hospitalsResult = await Hospitals.bulkCreate(hospitalsMock)
+      expect(hospitalsResult.length).toBe(2)
+    
+    });
+  
+
+  });
+  
